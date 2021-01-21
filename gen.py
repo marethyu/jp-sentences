45c5b4567b5dcd36104235a88340db94b16461a4
marethyu <codingexpert123@gmail.com>
2021-01-20 23:01:48 -0800

    Improved internal links

--- gen.py
+++ gen.py
@@ -35,6 +35,8 @@ chapters = [
     'Making relative clauses'
 ]
 
+SVG = '<svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg>'
+
 internal_links = []
 
 ty_to_str = ['pattern', 'example', 'practice']
@@ -44,7 +46,7 @@ def gen_link(line_buf, ty):
     tag = f'{ty_to_str[ty]}{counters[ty]}'
     counters[ty] += 1
     internal_links.append(tag)
-    line_buf.append(f'<a id="{tag}"></a>\n')
+    line_buf.append(f'<a id="{tag}" href="#{tag}">{SVG}</a>\n')
 
 def write_text_subbox(line_buf, label, txt):
     line_buf.append('<div class="text-subbox">\n')
